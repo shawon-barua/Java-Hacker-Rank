1b4c2835341be0922c5a81533c536bafd5b59aa0
shawon-barua <shawon.cse.ku@gmail.com>
2016-01-03 14:33:12 +0600

    JAVA 1ST PROJECT
    This is basic understanding of java

---
+++ Office/src/charColleague/charColleague.java
@@ -0,0 +1,35 @@
+package charColleague;
+
+public class charColleague {
+
+	String name="shawon";
+	char grade='A';
+	int age = 26;
+	float height=5.5f;
+	double weight=59.4;
+	
+	public void print()
+	{
+		System.out.println("Name: "+name);
+		System.out.println("Grade: "+grade);
+		System.out.println("Age: "+age);
+		System.out.println("Height: "+height);
+		System.out.println("Weight: "+weight);
+	}
+	
+	public void upgrade()
+	{
+		age=27;
+		name="Bopu";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		charColleague Shawon=new charColleague();
+		Shawon.print();
+		charColleague Bopu=Shawon;
+		Bopu.upgrade();
+		Bopu.print();
+		
+	}
+
+}
